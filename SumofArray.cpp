c3343e0e454311c3cacb406610842951762f4599
varunbasude16 <varunbasude16@gmail.com>
2026-02-02 14:49:02 +0530

    Add files via upload

---
+++ SumofArray.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int sum(int a[],int n)
+{
+	int sum=0;
+	for(int i=0;i<n;i++)
+	sum+=a[i];
+	return sum;
+}
+
+int main()
+{
+	int ar[]={1,20,3,5,0};
+	int n=5;
+	cout<<sum(ar,n);
+	
+	return 0;
+}
