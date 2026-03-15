5e8f5d58b5b8ab1bea8320743b0047f01de9d2e3
varunbasude16 <varunbasude16@gmail.com>
2026-03-16 02:42:24 +0530

    Update quck sort using random.cpp

--- sorting/quck sort using random.cpp
+++ sorting/quck sort using random.cpp
@@ -1,65 +1,63 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
-void swap(int* a, int* b) {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+// Function to swap two elements
+void swap(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
-
+// Partition function
 int partition(int arr[], int low, int high) {
-    
-    int pivot = arr[high];
-    
-    int i = (low - 1);    
+    int pivot = arr[high];   // pivot is last element
+    int i = low - 1;
 
-    for (int j = low; j < high; j++) {
-        
-        if (arr[j] <= pivot) {
-            i++; 
-            swap(&arr[i], &arr[j]);
+    for(int j = low; j < high; j++) {
+        if(arr[j] <= pivot) {
+            i++;
+            swap(arr[i], arr[j]);
         }
     }
-    swap(&arr[i + 1], &arr[high]);
-    return (i + 1);
+
+    swap(arr[i + 1], arr[high]);
+    return i + 1;
 }
-int partition_random(int arr[] , int low , int high ){
-    int r=(low+high)/2;
-    swap(&arr[r],&arr[high]);
+
+// Function to choose random pivot
+int randomPartition(int arr[], int low, int high) {
+    int randomIndex = low + rand() % (high - low + 1);
+
+    // Swap random pivot with last element
+    swap(arr[randomIndex], arr[high]);
+
     return partition(arr, low, high);
-    
 }
 
+// Quick Sort function
 void quickSort(int arr[], int low, int high) {
-    if (low < high) {
-
-        int pi = partition_random(arr, low, high);
-
+    if(low < high) {
+        int pi = randomPartition(arr, low, high);
 
         quickSort(arr, low, pi - 1);
         quickSort(arr, pi + 1, high);
     }
 }
 
-
-void printArray(int arr[], int size) {
-    for (int i = 0; i < size; i++)
-        cout << arr[i] << " ";
-    cout << std::endl;
-}
-
+// Main function
 int main() {
-    int data[] = {8, 7, 2, 1, 0, 9, 6};
-    int n = sizeof(data) / sizeof(data[0]);
-    
-    cout << "Unsorted Array: ";
-    printArray(data, n);
-    
-    quickSort(data, 0, n - 1);
-    
-    cout << "Sorted Array:   ";
-    printArray(data, n);
-    
+    srand(time(0)); // Seed for random number
+
+    int arr[] = {10, 7, 8, 9, 1, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    quickSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    for(int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
     return 0;
 }
