c960d478a63abec0841e9f6a6539310ae6587001
varunbasude16 <varunbasude16@gmail.com>
2026-04-06 14:32:58 +0530

    Add files via upload

---
+++ stack/reverseVector.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+//#include<stack>
+using namespace std;
+
+int main(){
+	vector<int> myvec = {1,2,3,4,5};
+
+for(int value : myvec)
+    cout << value << " ";
+	
+	return 0;
+}
+
