f12290089a478daf4f7b6e671ed67ae71178a4f4
varunbasude16 <varunbasude16@gmail.com>
2026-04-06 14:34:29 +0530

    Implement vector reversal using stack
    Added a function to reverse a vector using a stack.

--- stack/reverseVector.cpp
+++ stack/reverseVector.cpp
@@ -1,14 +1,32 @@
-#include<iostream>
+#include <iostream>
 #include<vector>
-//#include<stack>
 using namespace std;
+#include<stack>
 
-int main(){
-	vector<int> myvec = {1,2,3,4,5};
+
+void reverse (vector<int>& myvec){
+    stack<int> s;
+     
+     for(int i=0;i<myvec.size();i++)
+    s.push(myvec[i]);
+    
+    for(int i=0;i<myvec.size();i++){
+    myvec[i]=s.top();
+    s.pop()
+    
+    }
+    
+}
+
+
+int main() {
+vector<int> myvec = {1,2,3,4,5};
 
 for(int value : myvec)
     cout << value << " ";
-	
-	return 0;
+ reverse(myvec);
+ cout<<endl;
+ for(int value : myvec)
+    cout << value << " ";
+ return 0;
 }
-
