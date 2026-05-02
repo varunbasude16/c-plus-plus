7c41a4f009cac51c775651c46ef95a16211701de
varunbasude16 <varunbasude16@gmail.com>
2026-05-03 01:03:08 +0530

    Update Level by Level Printing.cpp

--- trees/Level by Level Printing.cpp
+++ trees/Level by Level Printing.cpp
@@ -12,6 +12,37 @@ struct Node{
 		right=NULL;
 	}
 };
+Node* insert(Node* root, int value){
+	Node* newNode=new Node(value);
+	if(root==NULL){
+		root=newNode;
+		return root;
+	}
+	
+	queue<Node*> q;
+	q.push(root);
+	
+	while(q.size()>0){
+		Node* temp=q.front();
+		q.pop();
+		
+		if(temp->left==NULL){
+			temp->left=newNode;
+			return root;
+		}else {
+			q.push(temp->left);
+		}
+		if(temp->right==NULL){
+			temp->right=newNode;
+			return root;
+		}else {
+			q.push(temp->right);
+		}
+		
+	}
+	return root;
+	
+}
 void LBLprint(Node* root){
 	queue<Node*> q;
 	q.push(root);
@@ -45,12 +76,19 @@ void LBLprint(Node* root){
 
 int main(){
 	Node* root=new Node(1);
-	root->left=new Node(2);
-	root->right=new Node(3);
-	root->left->left=new Node(4);
-	root->left->right=new Node(5);
-	root->right->left=new Node(6);	
-	root->right->right=new Node(7);
+//	root->left=new Node(2);
+//	root->right=new Node(3);
+//	root->left->left=new Node(4);
+//	root->left->right=new Node(5);
+//	root->right->left=new Node(6);	
+//	root->right->right=new Node(7);
+
+	root=insert(root,2);
+	root=insert(root,3);
+	root=insert(root,4);	
+	root=insert(root,5);
+	root=insert(root,6);
+	root=insert(root,7);	
 	
 	LBLprint(root);
 	return 0;
