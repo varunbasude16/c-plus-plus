d34c8668da63a0687fe03e39844d6f7ae6c1d3c9
varunbasude16 <varunbasude16@gmail.com>
2026-05-20 02:20:27 +0530

    Add files via upload

---
+++ graphs/BFST_traversal.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<list>
+using namespace std;
+
+class Graph{
+    int V;
+    list<int> *l;
+
+public:
+    Graph(int V){
+        this->V=V;
+        l=new list<int> [V];     
+    }
+    void addEdge(int u, int v){
+        l[u].push_back(v);
+        l[v].push_back(u);
+    }
+    void BFS(int s){
+       // vector<bool> vis(V,false);
+       int vis[V]={0};
+        queue<int> q;
+
+        q.push(s);
+        vis[s]=1;
+
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+            cout<<u<<" ";
+            for(int v:l[u]){
+                if(!vis[v]){
+                    q.push(v);
+                    vis[v]=1;
+                }
+            }
+        }
+    }
+};
+int main(){
+    Graph g(5);
+
+    g.addEdge(0,1);
+    g.addEdge(1,2);
+    g.addEdge(1,3);
+    g.addEdge(2,4);
+    
+
+    g.BFS(0);
+  
+    
+    return 0;
+}
